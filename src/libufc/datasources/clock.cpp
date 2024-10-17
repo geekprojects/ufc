d2f0ab7ec2775cd01621958545f4bd2a62f02e03
Ian Parker <ian@geekprojects.com>
2024-10-17 12:10:00 +0100

    Add a Clock Data source so you can use your WinWing FCU as a clock!

---
+++ src/libufc/datasources/clock.cpp
@@ -0,0 +1,146 @@
+//
+// Created by Ian Parker on 16/10/2024.
+//
+
+#include "clock.h"
+
+#include <ctime>
+#include <unistd.h>
+
+#include "ufc/flightconnector.h"
+
+using namespace std;
+using namespace UFC;
+
+UFC_DATA_SOURCE(Clock, ClockDataSource)
+
+ClockDataSource::ClockDataSource(FlightConnector* fc) :
+    DataSource(fc, "Clock", 0)
+{
+}
+
+bool ClockDataSource::connect()
+{
+    return true;
+}
+
+void ClockDataSource::disconnect()
+{
+    m_running = false;
+}
+
+bool ClockDataSource::update()
+{
+    while (m_running)
+    {
+        AircraftState state = m_flightConnector->getState();
+
+        state.connected = true;
+
+        time_t timestamp;
+        time(&timestamp);
+
+        struct tm local_time = {};
+        localtime_r(&timestamp, &local_time);
+
+        int hour = local_time.tm_hour;
+        if (!m_24Hour && hour > 12)
+        {
+            hour -= 12;
+            state.autopilot.speedManaged = true;
+        }
+        else
+        {
+            state.autopilot.speedManaged = false;
+        }
+
+        int minutes = local_time.tm_min;
+
+        if (m_showSeconds)
+        {
+            state.autopilot.speed = hour * 10;
+            state.autopilot.speed += minutes / 10;
+
+            state.autopilot.heading = (minutes % 10) * 100;
+            state.autopilot.heading += local_time.tm_sec;
+        }
+        else
+        {
+            state.autopilot.speed = hour;
+            state.autopilot.heading = minutes;
+        }
+        state.autopilot.headingManaged = !(local_time.tm_sec % 2);
+
+        /*
+
+        state.autopilot.speed = local_time.tm_hour;
+
+        if (!m_24Hour && state.autopilot.speed > 12)
+        {
+            state.autopilot.speed -= 12;
+        }
+        else
+        {
+            state.autopilot.speedManaged = false;
+        }
+
+        if (m_showSeconds)
+        {
+            state.autopilot.heading = local_time.tm_sec;
+
+            time_t diff = timestamp - m_showSecondsTime;
+            if (diff >= 10)
+            {
+                m_showSeconds = false;
+            }
+            state.autopilot.headingManaged = false;
+        }
+        else
+        {
+            state.autopilot.heading = local_time.tm_min;
+            state.autopilot.headingManaged = local_time.tm_sec % 2;
+        }
+        */
+
+        if (m_showYear)
+        {
+            state.autopilot.altitude = local_time.tm_year + 1900;
+
+            time_t diff = timestamp - m_showYearTime;
+            if (diff >= 5)
+            {
+                m_showYear = false;
+            }
+        }
+        else
+        {
+            state.autopilot.altitude = local_time.tm_mday;
+        }
+        state.autopilot.verticalSpeed = (local_time.tm_mon + 1) * 100;
+
+        m_flightConnector->updateState(state);
+
+        this_thread::sleep_for(chrono::milliseconds(100));
+    }
+
+    return true;
+}
+
+void ClockDataSource::command(std::string command)
+{
+    log(DEBUG, "command: %s", command.c_str());
+    if (command == AUTOPILOT_ALTITUDE_MANAGE)
+    {
+        m_showYear = true;
+        m_showYearTime = time(nullptr);
+    }
+    else if (command == AUTOPILOT_HEADING_MANAGE)
+    {
+        m_showSeconds = !m_showSeconds;
+        //m_showSecondsTime = time(nullptr);
+    }
+    else if (command == AUTOPILOT_AIRSPEED_MANAGE)
+    {
+        m_24Hour = !m_24Hour;
+    }
+}
---
+++ src/libufc/datasources/clock.h
@@ -0,0 +1,31 @@
+//
+// Created by Ian Parker on 16/10/2024.
+//
+
+#ifndef CLOCK_H
+#define CLOCK_H
+
+#include <ufc/datasource.h>
+
+class ClockDataSource : public UFC::DataSource
+{
+ private:
+    bool m_24Hour = true;
+    bool m_showYear = false;
+    time_t m_showYearTime = 0;
+    bool m_showSeconds = false;
+    time_t m_showSecondsTime = 0;
+
+ public:
+    ClockDataSource(UFC::FlightConnector* flightConnector);
+
+    bool connect() override;
+    void disconnect() override;
+    bool update() override;
+
+    void command(std::string command) override;
+};
+
+
+
+#endif //CLOCK_H
