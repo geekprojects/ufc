d1aa45d518c92846d8e125c1477983e014db4d8f
Ian Parker <ian@geekprojects.com>
2024-11-27 18:39:38 +0000

    Use std::ranges::find_if

--- src/libufc/datasources/xplane/navdata/navdata.h
+++ src/libufc/datasources/xplane/navdata/navdata.h
@@ -24,14 +24,14 @@ class NavDataUtils
 
     static void ltrim(std::string &s)
     {
-        s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        s.erase(s.begin(), std::ranges::find_if(s.begin(), s.end(), [](unsigned char ch) {
             return !NavDataUtils::shouldTrim(ch);
         }));
     }
 
     static void rtrim(std::string &s)
     {
-        s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        s.erase(std::ranges::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
             return !NavDataUtils::shouldTrim(ch);
         }).base(), s.end());
     }
